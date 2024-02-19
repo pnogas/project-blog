97d91c6481a701f56580ce3128198c1f0ce33cb0
Paul Nogas <Paul.Nogas@soti.net>
2024-02-19 15:15:08 -0500

    Part3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -6,8 +6,18 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
 
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadBlogPost(params.postSlug);
+
+  return {
+    title: frontmatter.title,
+    description: frontmatter.abstract,
+  };
+}
+
 async function BlogPost({ params }) {
   const { frontmatter, content } = await loadBlogPost(params.postSlug);
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
