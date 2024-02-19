6fecefe74b876f665019ad7f66b2c47991dd8fe3
Paul Nogas <Paul.Nogas@soti.net>
2024-02-19 14:48:47 -0500

    Updates
    missed frontmatter due to carelessness

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -7,12 +7,15 @@ import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
 
 async function BlogPost({ params }) {
-  const blogPost = await loadBlogPost(params.postSlug);
+  const { frontmatter, content } = await loadBlogPost(params.postSlug);
   return (
     <article className={styles.wrapper}>
-      <BlogHero title="Example post!" publishedOn={new Date()} />
+      <BlogHero
+        title={frontmatter.title}
+        publishedOn={frontmatter.publishedOn}
+      />
       <div className={styles.page}>
-        <MDXRemote source={blogPost.content} />
+        <MDXRemote source={content} />
       </div>
     </article>
   );
